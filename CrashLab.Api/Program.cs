44a1bcf7143ff5741f62d442585fc8ca6db1fb12
Bruno <bruno.freschi2@gmail.com>
2026-04-22 17:39:06 -0300

    updat api

--- CrashLab.Api/Program.cs
+++ CrashLab.Api/Program.cs
@@ -1,17 +1,29 @@
+using CrashLab.Domain.Entities;
+using CrashLab.Domain.Services;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddLogging();
+builder.Services.AddScoped<DocumentProcessor>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.MapOpenApi();
+app.MapScalarApiReference();
+
+app.MapPost("/documents/process", (Document doc, DocumentProcessor processor) =>
 {
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-
-app.MapGet("/", () => "0.01");
+    try
+    {
+        processor.Process(doc);
+        return Results.Ok(new { message = "Processed successfully" });
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
 
 app.Run();
