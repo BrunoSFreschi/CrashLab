02ab7fb31d5c71645c53b1e1639e2edb6aa1c60d
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-05-01 22:51:46 -0300

    removed

--- CrashLab.Api/Program.cs
+++ CrashLab.Api/Program.cs
@@ -1,43 +1,6 @@
-using CrashLab.Domain.Entities;
-using CrashLab.Domain.Services;
-using Scalar.AspNetCore;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddOpenApi();
-builder.Services.AddLogging();
-builder.Services.AddScoped<DocumentProcessor>();
-
 var app = builder.Build();
 
-app.MapOpenApi();
-app.MapScalarApiReference();
-
-app.MapPost("/documents/process", (Document doc, DocumentProcessor processor) =>
-{
-    try
-    {
-        processor.Process(doc);
-        return Results.Ok(new { message = "Processed successfully" });
-    }
-    catch (Exception ex)
-    {
-        return Results.Problem(ex.Message);
-    }
-});
-
-app.MapPost("/documents/process-silent", (Document doc, DocumentProcessor processor) =>
-{
-    try
-    {
-        processor.Process(doc);
-    }
-    catch
-    {
-        // Intentionally swallowed for lab exercise.
-    }
-
-    return Results.Ok(new { message = "Processed successfully" });
-});
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
